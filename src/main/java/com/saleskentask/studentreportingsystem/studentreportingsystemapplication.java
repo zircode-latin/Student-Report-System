5f541637c524d5e1a7bedec9b9d5491afe474ac7
zircode-latin <82564131+zircode-latin@users.noreply.github.com>
2023-02-22 09:38:52 +0530

    Create studentreportingsystemapplication.java

---
+++ src/main/java/com/saleskentask/studentreportingsystem/studentreportingsystemapplication.java
@@ -0,0 +1,13 @@
+package com.saleskentask.studentreportingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentreportingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentreportingsystemApplication.class, args);
+	}
+
+}
