1dfec24ff501b2c60e3630e9c6695e19b32eedf8
zircode-latin <82564131+zircode-latin@users.noreply.github.com>
2023-02-22 09:49:15 +0530

    Create StudentreportingsystemApplicationTests.java

---
+++ src/test/java/com/saleskentask/studentreportingsystem/StudentreportingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saleskentask.studentreportingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentreportingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
